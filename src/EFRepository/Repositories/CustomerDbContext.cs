6f651d44eb7030d7b9c63ad46c646bbe1c403b41
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-20 15:45:08 +0700

    feat: add customer repository

---
+++ src/EFRepository/Repositories/CustomerDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EFRepository.Entities;
+using System.Data.Entity;
+
+namespace EFRepository.Repositories
+{
+    public class CustomerDbContext: DbContext
+    {
+        public CustomerDbContext()
+            : base("Data Source=localhost\\SQLEXPRESS;Initial Catalog=CustomerLib_Demin;Integrated Security=True")
+        {
+
+        }
+
+        public IDbSet<Customer> Customer { get; set; }
+        public IDbSet<Address> Addresses { get; set; }
+        public IDbSet<Note> Notes { get; set; }
+    }
+}
