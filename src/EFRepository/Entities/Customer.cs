74a4772ff9f0a85208275e8c72eeec11a094eca0
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-20 12:21:39 +0700

    first commit

---
+++ src/EFRepository/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFRepository.Entities
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [Phone]
+        public string PhoneNumber { get; set; }
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public decimal TotalPurchasesAmount { get; set; }
+    }
+}
