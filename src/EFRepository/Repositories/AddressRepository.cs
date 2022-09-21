9b9bd9b2dae14c61595f969e29bf615f295f8df7
FreydZig <zigmunt.freyd.95@gmail.com>
2022-09-21 13:45:41 +0700

    feat: CustomerController

--- src/EFRepository/Repositories/AddressRepository.cs
+++ src/EFRepository/Repositories/AddressRepository.cs
@@ -1,10 +1,5 @@
 ﻿using EFRepository.Entities;
 using EFRepository.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EFRepository.Repositories
 {
--- tests/EFRerpository.tests/Repositories/AddressRepositoryTests.cs
+++ tests/EFRerpository.tests/Repositories/AddressRepositoryTests.cs
@@ -1,10 +1,5 @@
 ﻿using EFRepository.Entities;
 using EFRepository.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace EFRerpository.tests.Repositories
